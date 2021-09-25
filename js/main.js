f15d36424f208c018b50424f2cbfd251ed488887
Agustin Taibo <taibocruz@gmail.com>
2021-09-25 18:14:58 -0300

    footer terminado

--- js/main.js
+++ js/main.js
@@ -45,3 +45,41 @@ function scrollHeader(){
 }
 window.addEventListener('scroll', scrollHeader)
 
+
+/*scroll up*/
+
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up')
+
+    if(this.scrollY >= 200){
+        scrollUp.classList.add('show-scroll')
+    }
+    else{
+        scrollUp.classList.remove('show-scroll')
+    }
+}
+
+window.addEventListener('scroll', scrollUp)
+
+
+/*scroll section active link*/
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
